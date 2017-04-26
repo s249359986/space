db407eca34cb87cb6d504d4f537bfcc6e4511ba1
sdh <249359986@qq.com>
2017-04-26 18:43:15 +0800

    完善音乐插件

--- btAudio/README.md
+++ btAudio/README.md
@@ -1,2 +1,16 @@
 #btAudio
 
+### 方法
+1. play      instance.data("api").play(seconds);t 事件（秒）
+2. pause      instance.data("api").pause();
+3. setSrc     instance.data("api").setSrc(src);src 音乐源
+4. setUnmuted     instance.data("api").setUnmuted();关闭静音
+5. setMuted      instance.data("api").setMuted();设置静音
+
+### 事件
+1. onended 音乐播放完后触发。（暂定时不触发）
+2. onplay 音乐开始播放触发。
+3. onpause 音乐开始播放触发。
+4. oncanplay 可以播放音频
+5. ontimeupdate 播放进度(当前时间/总时间，this)
+
--- btAudio/src/js/btAudio.js
+++ btAudio/src/js/btAudio.js
@@ -24,51 +24,114 @@ if ( window.jQuery || window.Zepto ) {
                     unselectedAllAfter:function (wrapObj,selectedObj) {
                     }
                 },
+                playList:[],
                 controls:false,
                 autoplay:false,
                 loop:false,
                 onended:null,
+                onerror:null,
+                onpause:null,
+                onplay:null,
+                oncanplay:null,
                 preload:false,
+                ontimeupdate:null,
                 src:""
             };
             var opts = $.extend(true,{}, defaultOptisns,options);
+    
             var _this=this;
             _this.options=opts;
             _this.elem=container;
-            function play() {
-                _this.play();
+            if(!opts.src)
+            {
+                if(opts.onended)
+                {
+                    opts.onended("src");
+                }
+            }
+            function play(seconds) {
+                _this.play(seconds);
             }
             function pause() {
                 _this.pause();
             }
+            function setSrc(src) {
+                _this.setSrc(src);
+            }
+            function setMuted() {
+                _this.setMuted(true);
+            }
+            function setUnmuted() {
+                _this.setMuted(false);
+            }
+        
             _this.loadView();
             _this.audioObj=document.getElementById(opts.audioId);
             _this.audioObj.onended=function () {
-              
                 if(opts.onended)
                 {
                     opts.onended(_this);
+              
+                }
+            }
+            _this.audioObj.onerror=function (e) {
+                if(opts.onerror)
+                {
+                    opts.onerror(e,_this);
+                }
+            }
+            _this.audioObj.onpause=function (e) {
+                if(opts.onpause)
+                {
+                    opts.onpause(e,_this);
+                }
+            }
+            _this.audioObj.onplay=function (e) {
+                if(opts.onplay)
+                {
+                    opts.onplay(e,_this);
+                }
+            }
+            _this.audioObj.oncanplay=function (e) {
+                if(opts.oncanplay)
+                {
+                    opts.oncanplay(e,_this);
+                }
+            }
+            _this.audioObj.ontimeupdate=function (e) {
+                if(opts.ontimeupdate)
+                {
+                    //music.currentTime/music.duration
+                    opts.ontimeupdate(_this.audioObj.currentTime/_this.audioObj.duration,_this);
                 }
             }
             return {
+                setUnmuted:setUnmuted,
+                setMuted:setMuted,
+                setSrc:setSrc,
                 pause:pause,
                 play:play
             };
         }
-        BtObj.prototype.play=function () {
+        BtObj.prototype.play=function (seconds) {
             var _this=this;
-            // var tempOption=_this.options;
-            // var tempAudioObj=document.getElementById(tempOption.audioId);
+            if(seconds)
+            {
+                _this.audioObj.currentTime=seconds;
+            }
             _this.audioObj.play();
         };
         BtObj.prototype.pause=function () {
             var _this=this;
-            // var tempOption=_this.options;
-            // var tempAudioObj=document.getElementById(tempOption.audioId);
             _this.audioObj.pause();
         };
-        BtObj.prototype.setSrc=function () {
-
+        BtObj.prototype.setSrc=function (src) {
+            var _this=this;
+            _this.audioObj.src=src;
+        };
+        BtObj.prototype.setMuted=function (b) {
+            var _this=this;
+            _this.audioObj.muted=b;
         };
         BtObj.prototype.loadView=function () {
             var _this=this;
@@ -77,6 +140,10 @@ if ( window.jQuery || window.Zepto ) {
             var attrControls=tempOpt.controls?"controls":"";
             var attrLoop=tempOpt.loop?"loop":"";
             var attrPreload=tempOpt.preload?"preload":"";
+            if(!tempOpt.src)
+            {
+                throw new Error("src 不能为空");
+            }
             var attrSrc="src="+tempOpt.src;
             var attrId="id="+tempOpt.audioId;
             var tempAttr=attrAutoplay+" "+attrControls+" "+attrLoop+" "+attrPreload+" "+attrSrc+" "+attrId;
@@ -85,7 +152,6 @@ if ( window.jQuery || window.Zepto ) {
         };
         $.fn.btAudio = function(params) {
             return this.each(function(index,ele) {
-
                var tempId=ele.className.split(" ")[0]+ele.id+index;
                 $(this).data('api', new BtObj($(this), params,tempId));
             });
