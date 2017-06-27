cb6b0777de3c0d809d1d4d813a3950c84535e67e
宋冬红 <249359986@qq.com>
2017-06-27 18:19:56 +0800

    增加是否为整数

--- validate/src/js/validateRex.js
+++ validate/src/js/validateRex.js
@@ -33,18 +33,28 @@ function isEmail(s) {
     }
 }
 /*
+ 用途：整数
+ 输入：s：任意
+ 返回：如果通过验证返回true,否则返回false
+ */
+
+function isInteger(obj) {
+ return typeof obj === 'number' && obj%1 === 0
+}
+/*
  用途：手机号码
  输入：s：字符串
  返回：如果通过验证返回true,否则返回false
  */
 function isMobile(s) {
-    var re1= /^0{0,1}(19[0-9]|18[0-9]|17[0-9]|16[0-9]|11[0-9]|12[0-9]|13[0-9]|15[0-9]|18[6-9])[0-9]{8}$/;//11位
-    var re = /^1\d{10}$/;
-    if (re.test(s)) {
+    // var re1= /^0{0,1}(19[0-9]|18[0-9]|17[0-9]|16[0-9]|11[0-9]|12[0-9]|13[0-9]|15[0-9]|18[6-9])[0-9]{8}$/;//11位
+    // var re = /^1\d{10}$/;
+var reTel=/^1[3|4|5|7|8][0-9]\d{4,8}$/;//
+    if (reTel.test(s)) {
         return true;
     } else {
 
         return false;
     }
-    
+
 }
